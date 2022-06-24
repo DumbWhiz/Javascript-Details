47a1fb0f024e1bb73da1cb27bff4b260fea93597
DumbWhiz <adeyeyewilson@gmail.com>
2022-06-24 17:16:13 +0100

    Basic Javascript
    Alerting Name, height and Country with Javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstname = 'Wilson';
+let middlename = 'Ademola';
+let lastname = 'Adeyeye';
+let height = '5\'Something';
+let country = 'Nigeria';
+
+if (name = firstname + ' ' + lastname + ' ' + middlename){
+    alert ('Name:' + ' ' + name + '\n' + 'Height:' + ' '+ height + '\n' + 'Country:' + ' '+ country);
+    
+}
